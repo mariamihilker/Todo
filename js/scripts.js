0b062e36df021af9d7ca578b9f3a251de0c8cddb
mtatanashvili <mt@fuf.de>
2020-11-14 16:30:50 +0100

    Add Done function
    Add Done function

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //Call main
 loadTodo();
-
+toggleTodo();
 //Main Function
 function loadTodo() {
     document.querySelector('.todo-form').addEventListener('submit',todoAdd);
@@ -21,10 +21,18 @@ function todoAdd(e) {
 
 }
 
-function activeItem() {
-    let todoList = document.querySelector('.items-list li');
-    todoList.forEach(function(item) {
-        item.classList.add('active');
-    })
+function toggleTodo() {
+    document.querySelector('.items-list').addEventListener('click',clickToggle);
+}
+
+function clickToggle(e) {
+    console.log(e.target.parentElement)
+    if(e.target.parentElement.classList.contains('active')) {
+        e.target.parentElement.classList.add('done');
+        e.target.parentElement.classList.remove('active');
+    } else if(e.target.parentElement.classList.contains('done')) {
+        e.target.parentElement.classList.add('active');
+        e.target.parentElement.classList.remove('done');
+    }
 }
 
